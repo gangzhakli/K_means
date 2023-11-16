075aace7867ac216ce6797b66244ffbccddbac47
gangzhakli <gangzhakli@gmail.com>
2023-11-16 23:37:36 +0800

    2023-11-16 23:37:36

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ from private_gpt.server.embeddings.embeddings_router import (
 def test_embeddings_generation(test_client: TestClient) -> None:
     body = EmbeddingsBody(input="Embed me")
     response = test_client.post("/v1/embeddings", json=body.model_dump())
+
