43ffb79f4b04a84f6d707ee2305d08ff0b7f3663
gangzhakli <gangzhakli@gmail.com>
2024-05-27 18:38:33 +0800

    2024-05-27 18:38:33

--- main.py
+++ main.py
@@ -150,3 +150,4 @@ ui:
 def test_embeddings_generation(test_client: TestClient) -> None:
     body = EmbeddingsBody(input="Embed me")
     response = test_client.post("/v1/embeddings", json=body.model_dump())
+
