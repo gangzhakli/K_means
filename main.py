4ba0c9f953b2d9f98a7c68ab254baee10e158ecf
gangzhakli <gangzhakli@gmail.com>
2024-08-19 00:43:00 +0800

    2024-08-19 00:43:00

--- main.py
+++ main.py
@@ -223,3 +223,4 @@ from private_gpt.di import root_injector
 from private_gpt.server.ingest.ingest_service import IngestService
 from private_gpt.server.ingest.ingest_watcher import IngestWatcher
 
+logger = logging.getLogger(__name__)
