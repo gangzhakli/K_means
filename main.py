54c8aae4cece74e840bf6b169178a24cb64c063c
gangzhakli <gangzhakli@gmail.com>
2025-05-30 22:32:43 +0800

    2025-05-30 22:32:43

--- main.py
+++ main.py
@@ -451,3 +451,4 @@ def test_embeddings_generation(test_client: TestClient) -> None:
     assert response.status_code == 200
     embedding_response = EmbeddingsResponse.model_validate(response.json())
     assert len(embedding_response.data) > 0
+    assert len(embedding_response.data[0].embedding) > 0server:
